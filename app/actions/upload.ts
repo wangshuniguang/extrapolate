6c0f1b36f24bfe1ac06287c2851d98cbbec6ea16
wangshu <wangshunext@gmail.com>
2024-05-28 01:29:51 +0800

    update it

--- app/actions/upload.ts
+++ app/actions/upload.ts
@@ -1,7 +1,5 @@
 "use server";
 
-import Replicate from "replicate";
-import { createAdminClient } from "@/lib/supabase/admin";
 import { cookies } from "next/headers";
 import { createClient } from "@/lib/supabase/server";
 import { nanoid } from "nanoid";
@@ -9,74 +7,52 @@ import { redirect } from "next/navigation";
 // import { waitUntil } from "@vercel/functions";
 
 export async function upload(previousState: any, formData: FormData) {
-  // Authenticate
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
   const user_id = 'wangshu_next_test';
-  const supabaseAdmin = createAdminClient();
+
+  console.log('Fields:' + formData + ', keys: ' + 
+    formData.keys + ', values: ' + formData.values);
+
 
   const image = formData.get("image") as File;
   if (!image) {
     return { message: "Missing image", status: 400 };
   }
+
+  console.info('image: ' + image);
   
   const audio = formData.get("audio") as File;
   if (!audio) {
     return { message: "Missing audio", status: 400 };
   }
 
-  // Handle request
-  // Generate key and insert id to supabase
-  const { key } = await setRandomKey(user_id);
-  const input = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/input/${user_id}/${key}`;
+  console.info('audio: ' + audio);
 
+  const postFormData = new FormData();
+  postFormData.append('image', image);
+  postFormData.append('audio', audio);
 
-  const { data: storageData, error: storageError } = await supabaseAdmin.storage
-    .from("input")
-    .upload(`/${user_id}/${key}`, image, {
-      contentType: image.type,
-      cacheControl: "3600",
-      upsert: true,
+  console.log('start post request.');
+
+  try {
+    const response = await fetch('http://localhost:8000/api/file/upload', {
+      method: 'POST',
+      body: postFormData,
     });
 
-  if (storageError)
-    return {
-      message: "Unexpected error uploading image, please try again",
-      status: 400,
-    };
+    console.log('response: ' + response.status);
 
-  const { data: audioStorageData, error: audioStorageError } = await supabaseAdmin.storage
-    .from("input")
-    .upload(`/${user_id}/${key}`, audio, {
-      contentType: audio.type,
-      cacheControl: "3600",
-      upsert: true,
-    });
-
-  if (audioStorageError)
-    return {
-      message: "Unexpected error uploading audio, please try again",
-      status: 400,
-    };
-
-  redirect(`/p/${key}`);
-}
-
-// Generates new key that doesn't already exist in db
-async function setRandomKey(user_id: string): Promise<{ key: string }> {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
-
-  /* recursively set link till successful */
-  const key = nanoid();
-  const { error } = await supabase.from("data").insert({
-    id: key,
-    input: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/input/${user_id}/${key}`,
-  });
-  if (error) {
-    // by the off chance that key already exists
-    return setRandomKey(user_id);
-  } else {
-    return { key };
+    if (response.ok) {
+      const result = await response.json();
+      console.log('Files uploaded successfully:', result);
+      // Handle success
+    } else {
+      const errorText = await response.text();
+      console.error(`HTTP error! status: ${response.status}, message: ${errorText}`);
+      throw new Error(`HTTP error! status: ${response.status}, message: ${errorText}`);
+    }
+  } catch (error) {
+    console.error('Error uploading files:', error);
   }
+
+  redirect(`/`);
 }
